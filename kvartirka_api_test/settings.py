1f459728b1094640bcd94023cb53232f108e27dc
ma4akk <ma4akk@gmail.com>
2022-04-19 21:01:39 +0300

    add dj-database-url

--- kvartirka_api_test/settings.py
+++ kvartirka_api_test/settings.py
@@ -87,6 +87,12 @@ DATABASES = {
     }
 }
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
